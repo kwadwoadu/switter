755fd10938cc456eaa636a789413c4664c09aaf0
Kwadwo Adu <k@adu.dk>
2012-10-07 22:52:36 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Switter::Application.config.secret_token = 'db95c4af235ea1ba6820cd57769ecb4e28f1da86ca108317ba9d96e3ef697ab6a8ff507d4f69aaeb4203a509cbb921b348e69df099c0f852ac588297bcc3070a'
